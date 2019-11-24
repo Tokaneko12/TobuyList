f8f30880adb38ed8c7e4d66068c1d4a2c82f6449
金子俊樹 <kanekotoshiki@kanekotoshikis-MacBook-Pro.local>
2019-11-25 01:22:03 +0900

    firebase設定追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,16 @@
-// App logic.
-window.myApp = {};
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCAOHjxIIAOzg9J4kXSXvAn4LZlrD-oqcM",
+  authDomain: "tobuy-86979.firebaseapp.com",
+  databaseURL: "https://tobuy-86979.firebaseio.com",
+  projectId: "tobuy-86979",
+  storageBucket: "tobuy-86979.appspot.com",
+  messagingSenderId: "244020507559",
+  appId: "1:244020507559:web:b991b6dfb50faf62007c82",
+  measurementId: "G-1SM05G4STM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-document.addEventListener('init', function(event) {
-  var page = event.target;
-
-  // Each page calls its own initialization controller.
-  if (myApp.controllers.hasOwnProperty(page.id)) {
-    myApp.controllers[page.id](page);
-  }
-});
+var db = firebase.firestore();
