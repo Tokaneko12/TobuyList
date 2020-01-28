1c095b634e4516a7ab85880952f0c0b25004b6a3
unknown <scientia0502@gmail.com>
2020-01-29 02:00:48 +0900

    db周り整備

--- src/js/buydiary.js
+++ src/js/buydiary.js
@@ -1,3 +1,18 @@
-Module.controller('buyDiaryController', function() {
+Module.controller('buyDiaryController', ['$scope', function($scope) {
   var $ctrl = this;
-});
+
+  // 日記データ読み込み
+  $ctrl.initialize = function() {
+    $ctrl.allBuyRecord = [];
+    var buyItemsRef = db.collection("buyItems");
+    console.log(authUser.uid);
+    buyItemsRef.where("uid", "==", authUser.uid).get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        $ctrl.allBuyRecord.push(doc.data());
+        $scope.$apply();
+      });
+    })
+  }
+}]);
