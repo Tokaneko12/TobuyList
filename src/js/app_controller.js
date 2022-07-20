ae7ff3120d333cb39ea787f2a27d75b4717ad853
Tokaneko12 <scientia0502@gmail.com>
2022-07-20 17:17:27 +0900

    不具合修正対応

--- src/js/app_controller.js
+++ src/js/app_controller.js
@@ -6,4 +6,9 @@ Module.controller('appController', ['$scope',function($scope) {
    $app.dispSafeBtm = true;
  }
 
+ if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
+    // <html> 要素に属性を追加（値として空文字列を設定）
+    document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+  }
+
 }]);
